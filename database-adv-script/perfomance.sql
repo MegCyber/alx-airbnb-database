3048bddf99aa9490bae9f97ee9709480acab7b53
megCyber <m.olowokere@rgu.ac.uk>
2025-09-14 16:56:55 +0100

    week 2 Task third commit

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -257,7 +257,7 @@ FROM Booking b
 INNER JOIN User u ON b.user_id = u.user_id
 INNER JOIN Property p ON b.property_id = p.property_id
 INNER JOIN Payment pay ON b.booking_id = pay.booking_id
-
+EXPLAIN
 UNION ALL
 
 SELECT 
